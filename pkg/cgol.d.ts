3644ba7e7a8aa5bb9b0f96ff2113e4fddc916ca4
Anicet Nougaret <anicet.nougaret@zohomail.eu>
2021-07-06 16:08:43 +0200

    added builds

---
+++ pkg/cgol.d.ts
@@ -0,0 +1,50 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+*/
+export enum Cell {
+  Dead,
+  Alive,
+}
+/**
+*/
+export class Universe {
+  free(): void;
+/**
+* @param {number} width
+* @param {number} height
+* @returns {Universe}
+*/
+  static new(width: number, height: number): Universe;
+/**
+*/
+  tick(): void;
+/**
+* @returns {string}
+*/
+  render(): string;
+}
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly __wbg_universe_free: (a: number) => void;
+  readonly universe_new: (a: number, b: number) => number;
+  readonly universe_tick: (a: number) => void;
+  readonly universe_render: (a: number, b: number) => void;
+  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+  readonly __wbindgen_free: (a: number, b: number) => void;
+  readonly __wbindgen_malloc: (a: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
+}
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
